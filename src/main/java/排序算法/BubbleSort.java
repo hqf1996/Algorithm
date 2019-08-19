6fe04f90106f801f50be8c8248287f917bb40888
hqf1996 <378938941@qq.com>
2019-08-19 23:30:22 +0800

    冒泡排序

---
+++ src/main/java/排序算法/BubbleSort.java
@@ -0,0 +1,34 @@
+package 排序算法;
+
+import java.util.Arrays;
+
+/**
+ * @Author: hqf
+ * @description:
+ * @Data: Create in 23:15 2019/8/19
+ * @Modified By:
+ */
+public class BubbleSort {
+    public static int[] bubbleSort(int []a){
+        if (a.length == 0) {
+            System.out.println("需要长度大于0的数组");
+            return null;
+        }
+        for (int i = 0 ; i < a.length ; ++i){
+            for (int j = 0 ; j < a.length-i-1 ; ++j){
+                if (a[j] > a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int []a = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int[] sort_a = bubbleSort(a);
+        System.out.println(Arrays.toString(sort_a));
+    }
+}
